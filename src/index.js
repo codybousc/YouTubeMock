1c6c3bb1b2c25b79d1d0df380def902996ed6038
Cody Bouscaren <codybousc@gmail.com>
2016-03-18 17:51:20 -0400

    Iterates over API response

--- src/index.js
+++ src/index.js
@@ -2,20 +2,28 @@ import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
 import SearchBar from './components/search_bar';
 import YTSearch from 'youtube-api-search';
+import VideoList from './components/video_list';
 
 const API_KEY = 'AIzaSyCjXuQOPTrrQsCRG9_13GPaBjXqpvWcm5Y';
 
-YTSearch({ key: API_KEY, term: 'surfer girls'}, function(data) {
-  console.log(data);
-});
 
 //Create a new component which should produce some html
 //const App is a class and not an instance. there can be many instances of App
 class App extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = { videos: []};
+
+    YTSearch({ key: API_KEY, term: 'surfer girls'}, (videos) => {
+      this.setState({videos})
+    });
+  }
   render () {
     return (
       <div>
         <SearchBar />
+        <VideoList videos={this.state.videos}/>
       </div>
     );
   }
